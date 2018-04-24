fc9a3f52414264a06c951b52075506660b66cd46
ychost <ychost@outlook.com>
2018-04-24 19:15:23 +0800

    update: 2018-4-24 19:15:22

--- remoteBuild.bat
+++ remoteBuild.bat
@@ -1,1 +1,2 @@
+sw-precache --config=./sw-precache.js 
 bundle exec jekyll build 
---
+++ sw-precache.js
@@ -0,0 +1,13 @@
+﻿const prefix = '_site';
+
+module.exports = {
+  staticFileGlobs: [
+   
+     prefix + '/**/**.html',     // 所有页面，文章页面的html（必须包含）
+     prefix + '/assets/**/*',
+     prefix + '/files/**/*',
+     prefix + '/images/**/*',
+     '**//cdn.bootcss.com/**/*',
+  ],
+  stripPrefix: prefix
+}
